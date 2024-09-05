f3dc5afa9ce49c36eae9e6d4529edda0213ddc70
0ssamaak0 <0ssamaak0@gmail.com>
2024-09-06 01:06:10 +0300

    Comparing Duplicates between both datasets

--- duplicates/dataset_embeddings.py
+++ duplicates/dataset_embeddings.py
@@ -24,41 +24,7 @@ definitions, EMOTIONS, RANDOM_SEED = (
 id2label = {str(id): label for id, label in enumerate(EMOTIONS)}
 label2id = {v: k for k, v in id2label.items()}
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-def get_dataset_collection(vector_db_path):
-    """
-    Returns a collection from the ChromaDB client.
-
-    Args:
-        vector_db_path (str): The path to the vector database.
-
-    Returns:
-        chromadb.Collection: The collection object.
-    """
-    chroma_client = chromadb.PersistentClient(vector_db_path)
-    collection = chroma_client.get_or_create_collection(
-        name="goemotions_train", metadata={"hnsw:space": "cosine"}
-    )
-    return collection
-
-
-def get_dataframe(split="train"):
-    """
-    Loads the 'go_emotions' dataset, converts it to a pandas DataFrame, and adds a 'labels_text' column.
-
-    Args:
-        split (str, optional): The split of the dataset to load. Defaults to "train".
-
-    Returns:
-        pandas.DataFrame: The loaded dataset with an additional 'labels_text' column.
-    """
-    dataset = load_dataset("go_emotions", "simplified")
-    dataset.set_format(type="pandas")
-    df_train = dataset[split][:]
-    int2label = lambda x: dataset[split].features["labels"].feature.int2str(x)
-    df_train["labels_text"] = df_train["labels"].progress_apply(int2label)
-    return df_train
+device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 
 
 def get_embeddings(df, collection, idx=0):
@@ -118,69 +84,15 @@ def get_similars(df, collection, idx=0, n_results=5, verbose=False):
                 print(f"text: {text}\nlabels: {labels}\n distance: {distance:0.2f}\n")
     return matches
 
-
-# LEGACY CODE
-def get_semantic_statstical_distance(emotions1, emotions2, verbose=False):
-    """
-    Get the semantic statistical distance between two sets of emotions.
-
-    Args:
-        emotions1 (list): The first set of emotions.
-        emotions2 (list): The second set of emotions.
-        verbose (bool): Whether to print the distances.
-
-    Returns:
-        float: The semantic statistical distance between the two sets of emotions.
-    """
-    # Load emotion distances from CSV file
-    distances = pd.read_csv("combined_emotion_distances.csv")
-
-    # make emotions1 the largest of the two
-    if len(emotions1) < len(emotions2):
-        emotions1, emotions2 = emotions2, emotions1
-
-    accumulation = 0
-
-    # set for unique pairs
-    traversed = set()
-    for emotion1 in emotions1:
-        for emotion2 in emotions2:
-            # if the two emotions have been found before, skip this step
-            if (emotion1, emotion2) in traversed or (emotion2, emotion1) in traversed:
-                continue
-            else:
-                traversed.add((emotion1, emotion2))
-                traversed.add((emotion2, emotion1))
-
-            # Get distance from emotion_distances dictionary
-            distance = 100000
-            for index, row in distances.iterrows():
-                if (row["Emotion_1"] == emotion1 and row["Emotion_2"] == emotion2) or (
-                    row["Emotion_1"] == emotion2 and row["Emotion_2"] == emotion1
-                ):
-                    distance = row["Distance"]
-                    break
-            if distance == 100000:
-                print(
-                    f"Distance between {emotion1} and {emotion2} not found in the CSV file"
-                )
-
-            if verbose:
-                print(f"{emotion1} -> {emotion2}: {distance}")
-            accumulation += distance
-
-    return accumulation
-
-
 # Create Embeddings Database code
 def init_model():
-    # TODO replace with Ollama or fireworks embeddings
     tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
     model = AutoModel.from_pretrained(
         "nomic-ai/nomic-embed-text-v1.5", trust_remote_code=True
     )
     model.eval()
     model = model.to(device)
+    print(f"model running on {device}")
     return tokenizer, model
 
 
@@ -194,19 +106,28 @@ def mean_pooling(model_output, attention_mask):
     )
 
 
-def get_text_embeddings(text, model, tokenizer, norm=False):
+def get_text_embeddings(text, model, tokenizer, norm=False, batch = False):
     """
     Gets the text embeddings for the given text.
 
     Args:
         text (str): The text to get embeddings for.
+        model (transformers.PreTrainedModel): The model to use.
+        tokenizer (transformers.PreTrainedTokenizer): The tokenizer to use.
+        norm (bool, optional): Whether to normalize the embeddings. Defaults to False.
+        batch (bool, optional): Whether to process the text as a batch. Defaults to False.
 
     Returns:
         list: The text embeddings as a list.
     """
-    encoded_input = tokenizer(
-        [text], padding=True, truncation=True, return_tensors="pt"
-    ).to(device)
+    if batch:
+        encoded_input = tokenizer(
+            text, padding=True, truncation=True, return_tensors="pt"
+        ).to(device)
+    else:
+        encoded_input = tokenizer(
+            [text], padding=True, truncation=True, return_tensors="pt"
+        ).to(device)
     with torch.no_grad():
         model_output = model(**encoded_input)
 
@@ -216,21 +137,54 @@ def get_text_embeddings(text, model, tokenizer, norm=False):
     return embeddings.cpu().squeeze(0).numpy().tolist()
 
 
-def init_collection():
-    chroma_client = chromadb.PersistentClient("db/")
-    collection = chroma_client.get_or_create_collection(
-        name="emotions_cosine", metadata={"hnsw:space": "cosine"}
-    )
-    return collection
+# # LEGACY CODE
+# def get_semantic_statstical_distance(emotions1, emotions2, verbose=False):
+#     """
+#     Get the semantic statistical distance between two sets of emotions.
 
+#     Args:
+#         emotions1 (list): The first set of emotions.
+#         emotions2 (list): The second set of emotions.
+#         verbose (bool): Whether to print the distances.
 
-def insert_emotion_embeddings(model, tokenizer, definitions, label2id, collection):
-    """
-    Inserts the emotion embeddings into the collection.
-    """
-    for emotion in EMOTIONS:
-        embedding = get_text_embeddings(definitions[emotion], model, tokenizer)
-        collection.upsert(
-            ids=label2id[emotion],
-            embeddings=embedding,
-        )
+#     Returns:
+#         float: The semantic statistical distance between the two sets of emotions.
+#     """
+#     # Load emotion distances from CSV file
+#     distances = pd.read_csv("combined_emotion_distances.csv")
+
+#     # make emotions1 the largest of the two
+#     if len(emotions1) < len(emotions2):
+#         emotions1, emotions2 = emotions2, emotions1
+
+#     accumulation = 0
+
+#     # set for unique pairs
+#     traversed = set()
+#     for emotion1 in emotions1:
+#         for emotion2 in emotions2:
+#             # if the two emotions have been found before, skip this step
+#             if (emotion1, emotion2) in traversed or (emotion2, emotion1) in traversed:
+#                 continue
+#             else:
+#                 traversed.add((emotion1, emotion2))
+#                 traversed.add((emotion2, emotion1))
+
+#             # Get distance from emotion_distances dictionary
+#             distance = 100000
+#             for index, row in distances.iterrows():
+#                 if (row["Emotion_1"] == emotion1 and row["Emotion_2"] == emotion2) or (
+#                     row["Emotion_1"] == emotion2 and row["Emotion_2"] == emotion1
+#                 ):
+#                     distance = row["Distance"]
+#                     break
+#             if distance == 100000:
+#                 print(
+#                     f"Distance between {emotion1} and {emotion2} not found in the CSV file"
+#                 )
+
+#             if verbose:
+#                 print(f"{emotion1} -> {emotion2}: {distance}")
+#             accumulation += distance
+
+#     return accumulation
